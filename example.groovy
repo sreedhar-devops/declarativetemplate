878dda8d54afcdc5ebddfce487c5d31cff06a858
sreedhar-devops <39355121+sreedhar-devops@users.noreply.github.com>
2021-07-23 16:28:00 +0530

    change

--- example.groovy
+++ example.groovy
@@ -1,6 +1,11 @@
 def smoketestfun(String smoketestChoice, String deploymentEnvironment){
     echo "${smoketestChoice}"
     echo "${deploymentEnvironment}"
+    if(${SmokeTest} == "Yes") {
+        if( !fileExists("/var/lib/jenkins/smoketest") ) {         
+                sh 'mkdir -p /var/lib/jenkins/smoketest'
+        } 
+    }   
 }
 
 return this
