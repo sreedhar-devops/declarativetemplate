4b1049fee27321e773280c54cb940deb7bb6871b
sreedhar-devops <39355121+sreedhar-devops@users.noreply.github.com>
2021-07-26 14:32:48 +0530

    change

--- example.groovy
+++ example.groovy
@@ -10,7 +10,14 @@ def smoketestfun(String smoketestChoice, String deploymentEnvironment){
         } 
         if(DEPLOY_ENV == "qa1") {
             echo "inside qa1 if block"
-        //    def status = sh(script:'curl -u $credsfordownsteamjob_USR:$credsfordownsteamjob_PSW  http://localhost:8080/job/smoketestqa1/lastBuild/api/json|jq .building', returnStdout:true).trim()
+            def status = sh(script:'curl -u $credsfordownsteamjob_USR:$credsfordownsteamjob_PSW  http://localhost:8080/job/smoketestqa1/lastBuild/api/json|jq .building', returnStdout:true).trim()
+            if ( status == "true" ) {
+                sh ''' echo 600 > /var/lib/jenkins/smoketest/delaysecondsqa1 '''
+            } 
+            else { 
+                sh ''' echo " deploy env is $DEPLOY_ENV "  '''
+                sh ''' curl -d "token=my-token&DEPLOY_ENV=$DEPLOY_ENV" -i -X POST "http://$credsfordownsteamjob_USR:$credsfordownsteamjob_PSW@localhost:8080/job/smoketestqa1/buildWithParameters" '''              
+            }                        
         }
         else{
 
