d47a9a9af52552367912d3aa4738d2c5b3c416e3
sreedhar-devops <39355121+sreedhar-devops@users.noreply.github.com>
2021-07-26 11:09:11 +0530

    change

--- example.groovy
+++ example.groovy
@@ -1,12 +1,18 @@
 def smoketestfun(String smoketestChoice, String deploymentEnvironment){
-    echo "${smoketestChoice}"
-    echo "${deploymentEnvironment}"
-    smokeTest="${smoketestChoice}"
+//    echo "${smoketestChoice}"
+//    echo "${deploymentEnvironment}"
+//    smokeTest="${smoketestChoice}"
     echo smokeTest
     if( smokeTest == "Yes" ) {
         if( !fileExists("/var/lib/jenkins/smoketest") ) {         
                 sh 'mkdir -p /var/lib/jenkins/smoketest'
         } 
+/*        if(Deploy_Env == "qa1") {
+            def status = sh(script:'curl -u $credsfordownsteamjob_USR:$credsfordownsteamjob_PSW  http://localhost:8080/job/smoketestqa1/lastBuild/api/json|jq .building', returnStdout:true).trim()
+        }
+        else{
+
+        }  */
     }   
 }
 
