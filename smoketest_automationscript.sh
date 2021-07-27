7aa664f0a2663349093a33c553c93f994c0dd220
sreedhar-devops <39355121+sreedhar-devops@users.noreply.github.com>
2021-07-27 06:22:42 +0530

    change

--- smoketest_automationscript.sh
+++ smoketest_automationscript.sh
@@ -12,20 +12,20 @@ then
     fi
     if [ DEPLOY_ENV = "qa1" ];
     then
-        status =`curl -u $credsfordownsteamjob_USR:$credsfordownsteamjob_PSW  http://localhost:8080/job/smoketestqa1/lastBuild/api/json|jq .building`
+        status =`curl -u $USERNAME:$PASSWORD  http://localhost:8080/job/smoketestqa1/lastBuild/api/json|jq .building`
         if [ status = "true" ];
         then
             echo 600 > /var/lib/jenkins/smoketest/delaysecondsqa1 
         else
-            curl -d "token=my-token&DEPLOY_ENV=$DEPLOY_ENV" -i -X POST "http://$credsfordownsteamjob_USR:$credsfordownsteamjob_PSW@localhost:8080/job/smoketestqa1/buildWithParameters"
+            curl -d "token=my-token&DEPLOY_ENV=$DEPLOY_ENV" -i -X POST "http://$USERNAME:$PASSWORD@localhost:8080/job/smoketestqa1/buildWithParameters"
         fi
     else
-        status =`curl -u $credsfordownsteamjob_USR:$credsfordownsteamjob_PSW  http://localhost:8080/job/smoketestqa2/lastBuild/api/json|jq .building`
+        status =`curl -u $USERNAME:$PASSWORD  http://localhost:8080/job/smoketestqa2/lastBuild/api/json|jq .building`
         if [ status = "true" ];
         then
             echo 600 > /var/lib/jenkins/smoketest/delaysecondsqa2 
         else
-            curl -d "token=my-token&DEPLOY_ENV=$DEPLOY_ENV" -i -X POST "http://$credsfordownsteamjob_USR:$credsfordownsteamjob_PSW@localhost:8080/job/smoketestqa2/buildWithParameters"
+            curl -d "token=my-token&DEPLOY_ENV=$DEPLOY_ENV" -i -X POST "http://$USERNAME:$PASSWORD@localhost:8080/job/smoketestqa2/buildWithParameters"
         fi    
         
     fi
